ca1af400a11acb20ce3ea3699e47fefe64427ce8
carter <zongu3317@gmail.com>
2022-04-01 10:06:29 +0800

    [added] MVC第一版簽入

---
+++ Net6Mvc/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// MapControllerRoute 用來建立單一路由。 單一路由的名稱 default 為 route。
+// 大部分具有控制器和 views 的應用程式都會使用類似于路由的 default 路由範本。
+// ** REST Api 應該使用 屬性路由。**
+// 路由是使用 UseRouting 和 UseEndpoints 中介軟體進行設定。
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
